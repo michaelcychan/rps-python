64cd5b9cd17417a9b8a38377c9bbce97127beb59
Michael Chan <michaelcychan@gmail.com>
2022-09-20 11:48:17 +0100

    add raise exception test

--- src/rps_match.py
+++ src/rps_match.py
@@ -1,2 +1,21 @@
 def rps_match(str1, str2):
-    return "rock"
+    win_map = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
+    if check_valid_input(str1) and check_valid_input(str2):
+        if str1 == str2:
+            return "tie"
+        elif win_map[str1] == str2:
+            return str1
+        else:
+            return str2
+    else:
+        try:
+            raise ValueError("Input was incorrect")
+        except ValueError:
+            print("Except Line of Error")
+            raise
+
+def check_valid_input(str):
+    if str != "rock" and str != "scissors" and str != "paper":
+        return False
+    else:
+        return True
--- tests/test_rps_match.py
+++ tests/test_rps_match.py
@@ -4,4 +4,19 @@ from src.rps_match import rps_match
 
 class TestRPS(unittest.TestCase):
     def test_rps_match_rock_scissors(self):
-      assert rps_match("rock", "scissors") == "rock"
+        assert rps_match("rock", "scissors") == "rock"
+
+    def test_rps_match_scissors_paper(self):
+        assert rps_match("scissors", "paper") == "scissors"
+
+    def test_rps_match_paper_scissors(self):
+        assert rps_match("paper", "scissors") == "scissors"
+
+    def test_rps_match_tie(self):
+        assert rps_match("scissors", "scissors") == "tie"
+
+    def test_rps_match_wrong_input(self):
+        with self.assertRaises(ValueError):
+            rps_match("worm", "rock")
+    
+    
